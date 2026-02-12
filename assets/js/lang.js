b4bbff9ef6fdb7482dfd07be5e54ff6856da0f82
Fanuel Debebe <fanueld3@gmail.com>
2026-02-12 19:34:42 +0300

    Update lang.js

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,1 +1,13 @@
-.
+function setLanguage(lang) {
+  document.querySelectorAll("[data-en]").forEach(el => {
+    el.textContent = el.getAttribute(`data-${lang}`);
+  });
+
+  localStorage.setItem("lang", lang);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedLang = localStorage.getItem("lang") || "en";
+  setLanguage(savedLang);
+});
+
