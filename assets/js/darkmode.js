ee851c55c7566287967b0128ee65c540feaec753
Fanuel Debebe <fanueld3@gmail.com>
2026-02-12 19:36:11 +0300

    Update darkmode.js

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,1 +1,11 @@
-.
+const toggleDark = () => {
+  document.body.classList.toggle("dark");
+  localStorage.setItem("darkMode", document.body.classList.contains("dark"));
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("darkMode") === "true") {
+    document.body.classList.add("dark");
+  }
+});
+
